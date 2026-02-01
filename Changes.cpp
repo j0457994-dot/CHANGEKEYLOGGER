30f88dd89c582fb14fad691e29cb28a487668c4a
j0457994-dot <j0457994@gmail.com>
2026-02-01 02:21:01 +0100

    Update Changes.cpp

--- Changes.cpp
+++ Changes.cpp
@@ -1,3 +1,10 @@
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCKAPI_   // Prevents inclusion of winsock.h in windows.h
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+// Then the rest of your includes...
+
 #include <windows.h>
 #include <winhttp.h>
 #include <string>
